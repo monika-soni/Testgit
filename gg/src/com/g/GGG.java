7653ccf7f9d167e662bf12c07a93fccb226cd6c4
Monika <Monika@Monika-PC>
2023-06-10 23:18:06 +0530

    rrrr

--- gg/src/com/g/GGG.java
+++ gg/src/com/g/GGG.java
@@ -5,8 +5,8 @@ public class GGG
 public static void main(String[] args)
 {
 	System.out.println("vvvvvvv");
-	System.out.println("TTTTTT");
+	System.out.println("TTT");
     System.out.println("pppp");
-	System.out.println("javaa");
+	System.out.println("java");
 }
 }
