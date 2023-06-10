152c7fb0dd14cf1b5298f60f6a93f704232de064
Monika <Monika@Monika-PC>
2023-06-10 23:41:01 +0530

    ooo

--- gg/src/com/g/GGG.java
+++ gg/src/com/g/GGG.java
@@ -7,6 +7,6 @@ public static void main(String[] args)
 	System.out.println("vvvvvvv");
 	System.out.println("TTT");
     System.out.println("pppp");
-	System.out.println("jav");
+	System.out.println("java");
 }
 }
