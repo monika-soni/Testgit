3f1f7b57cbeee90f5a2060983ae8601386356a26
Monika <Monika@Monika-PC>
2023-06-10 22:56:22 +0530

    uuuu

--- gg/src/com/g/GGG.java
+++ gg/src/com/g/GGG.java
@@ -6,7 +6,7 @@ public static void main(String[] args)
 {
 	System.out.println("vvvvvvv");
 	System.out.println("TTTTTT");
-	System.out.println("uur")
+	System.out.println("uur");
 	System.out.println("pppp");
 	System.out.println("javaa");
 }
